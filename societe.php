324a799c5296b5e4bb4294c9998abe5626f91913
JojiElTigre <johan.rispal515@gmail.com>
2019-05-17 16:02:15 +0200

    page societe

--- societe.php
+++ societe.php
@@ -19,14 +19,15 @@
 		</div>
 		<div id="presentation">
 			<h3>Presentation</h3>
-			<p>Qu'on soit d'accord, bientôt tout sera de la HD, et la HD que ça soit du poyer point, du excel ou du power point, c'est la même chose<br>
-			C'est le XTP c'est le collactron, c'est 44 euros</p>
+			<p>Qu'on soit d'accord, bientôt tout sera de la HD, et la HD que ça soit du poyer point, du excel ou du power point, c'est la même chose
 		</div>
 		<br>
 		<br>
 		<br>
 		<div id="valeur">
 			<h3>Nos Valeurs</h3>
-			<p>On vous dira souvent que pour faire de la bureautique le spectron suffira, mais on est pas d'accord c'est pas notre politique</p>
+			<p>On vous dira souvent que pour faire de la bureautique le spectron suffira, mais on est pas d'accord c'est pas notre politique
+			<br><br>
+			On a le XTP c'est le collactron, c'est 44 euros. Tout ça c'est dans la pupille</p></p>
 		</div>
 </html>
